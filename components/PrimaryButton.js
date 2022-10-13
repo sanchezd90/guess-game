e5fb03693c11273f22e2571aed047bdd50425a74
Daniel <daniel.rueda@ibisdev.tech>
2022-10-13 10:57:23 -0300

    custom buttons

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,9 +1,43 @@
-import {View,Text} from 'react-native'
+import {View,Text, Pressable, StyleSheet} from 'react-native'
 
 function PrimaryButton({children}) {
-    return <View>
-        <Text>{children}</Text>
+    function pressHanlder(){
+        console.log('pressed');
+    }
+
+    return (
+    <View style={styles.outerContainer}>
+        <Pressable 
+            style={({pressed})=> pressed ? [styles.innerContainer,styles.pressed] : styles.innerContainer} 
+            onPress={pressHanlder} 
+            android_ripple={{color:'#640233'}}
+        >    
+                <Text style={styles.buttonText}>{children}</Text>
+        </Pressable>
     </View>
+    )
 }
 
 export default PrimaryButton
+
+
+const styles = StyleSheet.create({ 
+    outerContainer:{
+        borderRadius:28,
+        margin:4,
+        overflow:'hidden'
+    },
+    innerContainer:{      
+      backgroundColor:'#72063c',
+      paddingVertical: 8,
+      paddingHorizontal: 16,
+      elevation: 2
+    },
+    buttonText: {
+        color: 'white',
+        textAlign:'center'
+    },
+    pressed:{
+        opacity:0.75 //for iOS button feedback
+    }   
+  });
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -25,7 +25,7 @@ const styles = StyleSheet.create({
       marginTop: 100,
       marginHorizontal:24,
       borderRadius: 8,
-      backgroundColor: '#72063c',
+      backgroundColor: '#4e0329',
       elevation:4,//android
       shadowColor: 'black', //iOS
       shadowOffset: {width:0,height:2}, //iOS
