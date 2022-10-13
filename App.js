d22773680002f0ab327faf0b7d0622fa2c8fb5c6
Daniel <daniel.rueda@ibisdev.tech>
2022-10-13 11:23:08 -0300

    background gradient

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet,View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
-    <StartGameScreen/>
+    <LinearGradient colors={['#3b021f','#ddb52f']} style={styles.rootScreen}>
+      <StartGameScreen/>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({ 
- 
+  rootScreen:{
+    flex:1,
+  }
 });
