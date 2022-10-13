93825981141a5b428019dee64cf5c4965777e43f
Daniel <daniel.rueda@ibisdev.tech>
2022-10-13 11:44:39 -0300

    background image gradient

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StyleSheet,View } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
     <LinearGradient colors={['#3b021f','#ddb52f']} style={styles.rootScreen}>
-      <StartGameScreen/>
+      <ImageBackground 
+        source={require('./assets/images/background.png')}
+        resizeMode='cover'//zooms image if needed
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+        >
+        <StartGameScreen/>
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -13,5 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({ 
   rootScreen:{
     flex:1,
+  },
+  backgroundImage:{
+    opacity:0.15
   }
 });
