95442aef056ccc0d6339cc143e18569255d2bf39
Daniel <daniel.rueda@ibisdev.tech>
2022-10-14 09:31:23 -0300

    custom tittle

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
-import { StyleSheet, ImageBackground } from 'react-native';
+import {useState} from 'react'
+import { StyleSheet, ImageBackground, SafeAreaView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
 
 export default function App() {
+  const [userNumber,setUserNumber] = useState()
+
+  function pickedGameHandler (pickedNumber) {
+    setUserNumber(pickedNumber)
+  }
+
+  let screen = <StartGameScreen onPickNumber={pickedGameHandler}/>
+
+  if (userNumber) {
+    screen = <GameScreen/>
+  }
+
   return (
     <LinearGradient colors={['#3b021f','#ddb52f']} style={styles.rootScreen}>
       <ImageBackground 
@@ -11,7 +25,9 @@ export default function App() {
         style={styles.rootScreen}
         imageStyle={styles.backgroundImage}
         >
-        <StartGameScreen/>
+          <SafeAreaView style={styles.rootScreen}>
+            {screen}
+          </SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   );
